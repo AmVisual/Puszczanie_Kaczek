a3805b7bf49386b07072a02485113c3a30652d1d
Maciej Standerski <01159859@pw.edu.pl>
2022-03-20 17:21:33 +0100

    Animation label

--- src/po/matmac/AnimationPanel.java
+++ src/po/matmac/AnimationPanel.java
@@ -1,14 +1,21 @@
 package po.matmac;
 
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.LayoutManager;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 //panel for the animation
 public class AnimationPanel extends JPanel {
 
 	public AnimationPanel() {
-		// TODO Auto-generated constructor stub
+		JLabel info = new JLabel("Animacja");
+		info.setHorizontalAlignment(JLabel.CENTER);
+		info.setFont(new Font("Calibri", Font.PLAIN, 24));
+		this.setLayout(new GridLayout(1,1));
+		this.add(info);
 	}
 
 	public AnimationPanel(LayoutManager layout) {
